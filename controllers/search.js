7f1f8bbaaec6494b1c87a8b7344fe2ad8bb8e2b2
kartiksingh01 <kskartiksingh22@gmail.com>
2022-02-05 17:03:51 +0530

    added pagination

--- controllers/search.js
+++ controllers/search.js
@@ -1,26 +1,35 @@
 const nanozymeSchema = require("../models/Nanozyme");
 exports.getSearchResults = async (req, res) => {
   const { name, pmid } = req.cookies.search;
-  nanozymeSchema.find(
-    {
-      $or: [{ name: { $regex: `${name}` } }, { pmid: { $regex: `${pmid}` } }],
-    },
-    (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json(data);
-      }
-    }
-  );
+  var perPage = 20;
+  var page = req.query.page || 1;
+  nanozymeSchema
+    .find(
+      {
+        $or: [{ name: { $regex: `${name}` } }, { pmid: { $regex: `${pmid}` } }],
+      },
+      // (err) => {
+      //   if (err) {
+      //     console.log(err);
+      //   }
+      // }
+    )
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec((err, data) => {
+      nanozymeSchema.count().exec((err, count) => {
+        if (err) return next(err);
+        res.json([data, count]);
+        // https://evdokimovm.github.io/javascript/nodejs/mongodb/pagination/expressjs/ejs/bootstrap/2017/08/20/create-pagination-with-nodejs-mongodb-express-and-ejs-step-by-step-from-scratch.html
+      });
+    });
 };
 exports.postSearchResults = async (req, res) => {
   res.clearCookie();
-  res.cookie("search", {name: req.body.name, pmid: req.body.pmid});
+  res.cookie("search", { name: req.body.name, pmid: req.body.pmid });
   // res.redirect("/search_result")
   res.json("created");
 };
-
 
 // exports.getSearchResults = async (req, res) => {
 //   const { name, pmid } = req.query;
